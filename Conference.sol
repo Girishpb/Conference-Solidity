dbbfd9af0534ffda6ee61f12822ebc111f1fb28a
Girish Bennur <115867099+Girishpb@users.noreply.github.com>
2023-05-25 21:49:36 +0530

    Update Conference.sol

--- Conference.sol
+++ Conference.sol
@@ -4,3 +4,12 @@ contract Conference {  // can be killed, so the owner gets sent the money in the
 	mapping (address => uint) public registrantsPaid;
 	uint public numRegistrants;
 	uint public quota;
+	
+	event Deposit(address _from, uint _amount); // so you can log the event
+	event Refund(address _to, uint _amount); // so you can log the event
+
+	function Conference() {
+		organizer = msg.sender;		
+		quota = 100;
+		numRegistrants = 0;
+	}
