70bfdc47ef3401bd7e0251f7c72a18224867a14b
Girish Bennur <115867099+Girishpb@users.noreply.github.com>
2023-05-25 23:14:22 +0530

    Update Conference.sol

--- Conference.sol
+++ Conference.sol
@@ -13,3 +13,16 @@ contract Conference {  // can be killed, so the owner gets sent the money in the
 		quota = 100;
 		numRegistrants = 0;
 	}
+	function buyTicket() public {
+		if (numRegistrants >= quota) { 
+			throw; // throw ensures funds will be returned
+		}
+		registrantsPaid[msg.sender] = msg.value;
+		numRegistrants++;
+		Deposit(msg.sender, msg.value);
+	}
+
+	function changeQuota(uint newquota) public {
+		if (msg.sender != organizer) { return; }
+		quota = newquota;
+	}
