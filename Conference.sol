81b42af39c4a47ddf15c6663d07e3fe915df7d18
Girish Bennur <115867099+Girishpb@users.noreply.github.com>
2023-05-25 21:49:04 +0530

    Update Conference.sol

--- Conference.sol
+++ Conference.sol
@@ -1,1 +1,6 @@
+contract Conference {  // can be killed, so the owner gets sent the money in the end
 
+	address public organizer;
+	mapping (address => uint) public registrantsPaid;
+	uint public numRegistrants;
+	uint public quota;
